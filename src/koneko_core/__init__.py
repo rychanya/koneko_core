f793c8e657cdd75d715c3dc6064bf0dc724ea136
Rychanya <rychanya@gmail.com>
2024-09-13 11:01:48 +0300

    init

--- src/koneko_core/__init__.py
+++ src/koneko_core/__init__.py
@@ -1,8 +1,40 @@
-class EventStore[Event]:
-    events: list[Event]
+from uuid import UUID
+
+
+class MissingParentError(Exception):
+    ...
+
+
+class ParentNotFoundError(Exception):
+    ...
+
+
+class EventNotLastError(Exception):
+    ...
+
+
+class Event:
+    uid: UUID
+    previous_uid: UUID | None
+    next_uid: UUID | None
+
+
+class EventStore:
+    last_event_uid: UUID
+    events: dict[UUID, Event]
 
     async def add(self, event: Event) -> None:
-        self.events.append(event)
+        if event.previous_uid is None:
+            if self.events:
+                raise MissingParentError
+            else:
+                self.events[event.uid] = event
+                return
 
-    async def get_last_events(self, count: int, last_event_id: int) -> list[Event]:
-        return self.events[last_event_id : last_event_id + count]
+        previous_event = self.events.get(event.previous_uid)
+        if previous_event is None:
+            raise ParentNotFoundError
+        if previous_event.next_uid is not None:
+            raise EventNotLastError
+        self.events[event.uid] = event
+        previous_event.next_uid = event.uid
--- tests/test_.py
+++ tests/test_.py
@@ -5,6 +5,6 @@ pytestmark = pytest.mark.anyio
 
 
 async def test_event_store() -> None:
-    store = EventStore[int]()
-    store.events = list(range(10))
-    assert await store.get_last_events(5, 2) == [2, 3, 4, 5, 6]
+    store = EventStore()
+    store.events = list(range(7))
+    assert await store.get_last_events(2) == [2, 3, 4, 5, 6]
