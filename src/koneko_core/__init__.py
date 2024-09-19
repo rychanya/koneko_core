e1c07076a7487932636dade2dc318fcac9128073
Rychanya <rychanya@gmail.com>
2024-09-19 10:55:39 +0300

    init

--- src/koneko_core/__init__.py
+++ src/koneko_core/__init__.py
@@ -1,40 +1,89 @@
+import abc
+from dataclasses import dataclass
 from uuid import UUID
 
 
-class BaseEventStoreError(Exception): ...
+class BaseEventStoreError(Exception):
+    ...
 
 
-class MissingParentError(BaseEventStoreError): ...
+class MissingParentError(BaseEventStoreError):
+    ...
 
 
-class ParentNotFoundError(BaseEventStoreError): ...
+class ParentNotFoundError(BaseEventStoreError):
+    ...
 
 
-class EventNotLastError(BaseEventStoreError): ...
+class EventNotLastError(BaseEventStoreError):
+    ...
 
 
+class EventNotExistError(BaseEventStoreError):
+    ...
+
+
+class NewEventHasNextError(BaseEventStoreError):
+    ...
+
+
+class EventConflictError(BaseEventStoreError):
+    ...
+
+
+@dataclass
 class Event:
     uid: UUID
     previous_uid: UUID | None
     next_uid: UUID | None
 
 
-class EventStore:
-    last_event_uid: UUID
-    events: dict[UUID, Event]
+class AbstractEventStore(abc.ABC):  # pragma: no cover
+    @abc.abstractmethod
+    async def add(self, event: Event) -> None:
+        ...
+
+    @abc.abstractmethod
+    async def get_event_by_id(self, event_id: UUID) -> Event:
+        ...
+
+    @abc.abstractmethod
+    async def get_last_event(self) -> Event:
+        ...
+
+
+class MemoryEventStore(AbstractEventStore):
+    def __init__(self) -> None:
+        self._events: dict[UUID, Event] = dict()
+        self._last_event_id: UUID | None = None
 
     async def add(self, event: Event) -> None:
+        if event.next_uid is not None:
+            raise NewEventHasNextError
         if event.previous_uid is None:
-            if self.events:
-                raise MissingParentError
-            else:
-                self.events[event.uid] = event
+            if self._last_event_id is None:
+                self._events[event.uid] = event
+                self._last_event_id = event.uid
                 return
-
-        previous_event = self.events.get(event.previous_uid)
-        if previous_event is None:
-            raise ParentNotFoundError
-        if previous_event.next_uid is not None:
+            else:
+                raise EventConflictError
+        try:
+            last_event = await self.get_last_event()
+        except EventNotExistError as error:
+            raise EventConflictError from error
+        if event.previous_uid != last_event.uid:
             raise EventNotLastError
-        self.events[event.uid] = event
-        previous_event.next_uid = event.uid
+        self._events[event.uid] = event
+        last_event.next_uid = event.uid
+        self._last_event_id = event.uid
+
+    async def get_event_by_id(self, event_id: UUID) -> Event:
+        try:
+            return self._events[event_id]
+        except KeyError as error:
+            raise EventNotExistError from error
+
+    async def get_last_event(self) -> Event:
+        if self._last_event_id is None:
+            raise EventNotExistError
+        return await self.get_event_by_id(event_id=self._last_event_id)
--- tests/test_.py
+++ tests/test_.py
@@ -1,10 +1,37 @@
+from uuid import uuid4
+
 import pytest
-from koneko_core import EventStore
+from koneko_core import Event, EventConflictError, EventNotExistError, MemoryEventStore, NewEventHasNextError
 
 pytestmark = pytest.mark.anyio
 
 
-async def test_event_store() -> None:
-    store = EventStore()
-    store.events = list(range(7))
-    assert await store.get_last_events(2) == [2, 3, 4, 5, 6]
+@pytest.fixture
+def empty_memory_event_store() -> MemoryEventStore:
+    return MemoryEventStore()
+
+
+@pytest.fixture
+def event() -> Event:
+    return Event(uid=uuid4(), previous_uid=None, next_uid=None)
+
+
+class TestEmptyEventStore:
+    async def test_get_last(self, empty_memory_event_store: MemoryEventStore) -> None:
+        with pytest.raises(EventNotExistError):
+            await empty_memory_event_store.get_last_event()
+
+    async def test_add(self, empty_memory_event_store: MemoryEventStore, event: Event) -> None:
+        await empty_memory_event_store.add(event)
+        last_event = await empty_memory_event_store.get_last_event()
+        assert last_event.uid == event.uid
+
+    async def test_add_with_previus(self, empty_memory_event_store: MemoryEventStore, event: Event) -> None:
+        event.previous_uid = uuid4()
+        with pytest.raises(EventConflictError):
+            await empty_memory_event_store.add(event)
+
+    async def test_add_with_next(self, empty_memory_event_store: MemoryEventStore, event: Event) -> None:
+        event.next_uid = uuid4()
+        with pytest.raises(NewEventHasNextError):
+            await empty_memory_event_store.add(event)
